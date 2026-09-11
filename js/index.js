1232edc4d7f4d08737cc8cdf27e27321d7f6f5e0
sajwan05 <surajsajwan328@gmail.com>
2026-09-11 15:47:04 +0530

    write function for computer choice

--- js/index.js
+++ js/index.js
@@ -5,6 +5,17 @@ const result = document.querySelector(".result");
 const computerScore = 0;
 const yourScore = 0;
 
+
+// to get bot Choice 
+
+function getComputerChoice(){
+    const choices = ["rock", "paper", "scissors"];
+
+    const randomIndex = Math.floor(Math.random() * choices.length);
+
+    return choices[randomIndex];
+}
+
 humanChoices.addEventListener("click", (e) => {
     const target = e.target;
     const value = target.classList.value;
@@ -27,6 +38,6 @@ humanChoices.addEventListener("click", (e) => {
             userChoice.textContent = yourChoice;
     }
 
-    
+    const computerChoice = getComputerChoice();
 
 });
