819a61d42353a23fe90ae11e8d31a4424d4389a4
sajwan05 <surajsajwan328@gmail.com>
2026-09-11 15:42:35 +0530

    add userChoice on screen with dom

--- js/index.js
+++ js/index.js
@@ -9,17 +9,24 @@ humanChoices.addEventListener("click", (e) => {
     const target = e.target;
     const value = target.classList.value;
 
+    const userChoice = document.querySelector(".user-choice");
+
     let yourChoice = "";
 
     switch(value){
         case "rock":
             yourChoice = value;
+            userChoice.textContent = yourChoice;
             break;
         case "scissors":
             yourChoice = value;
+            userChoice.textContent = yourChoice;
             break;
         case "paper":
             yourChoice = value;
+            userChoice.textContent = yourChoice;
     }
+
+    
 
 });
