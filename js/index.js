6679aa86826d9874105d55d0bcce0c14cdfa0d6e
sajwan05 <surajsajwan328@gmail.com>
2026-09-11 18:26:06 +0530

    final touches and some tweaks

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 console.log("Javascript is connected!");
 
+const main = document.querySelector("main");
 const humanChoices = document.querySelector(".human-choices");
 const output = document.querySelector(".result > .output");
 const humanScore = document.querySelector(".humanScore");
 const botScore = document.querySelector(".computerScore");
+const finalResult = document.querySelector(".final-result");
 let computerScore = 0;
 let yourScore = 0;
 
@@ -20,36 +22,58 @@ function getComputerChoice(){
 
 humanChoices.addEventListener("click", (e) => {
 
-    const target = e.target;
-    const value = target.classList.value;
-
+    const btn  = document.createElement("button");
     const userChoice = document.querySelector(".user-choice");
     const botChoice = document.querySelector(".bot-choice");
 
-    const computerChoice = getComputerChoice();
-    let yourChoice = "";
-
-    switch(value){
-        case "rock":
-            yourChoice = value;
-            userChoice.textContent = yourChoice;
-            break;
-        case "scissors":
-            yourChoice = value;
-            userChoice.textContent = yourChoice;
-            break;
-        case "paper":
-            yourChoice = value;
-            userChoice.textContent = yourChoice;
-    }
-
-    botChoice.textContent = computerChoice;
-
     if(yourScore < 5 && computerScore < 5){
+
+        const target = e.target;
+        const value = target.classList.value;
+
+        const computerChoice = getComputerChoice();
+        let yourChoice = "";
+
+        switch(value){
+            case "rock":
+                yourChoice = value;
+                userChoice.textContent = yourChoice;
+                break;
+            case "scissors":
+                yourChoice = value;
+                userChoice.textContent = yourChoice;
+                break;
+            case "paper":
+                yourChoice = value;
+                userChoice.textContent = yourChoice;
+        }
+
+        botChoice.textContent = computerChoice;
+
         playRound(yourChoice, computerChoice);
     }
 
+    
+    if(yourScore === 5 || computerScore === 5){
+        isWinner();
 
+        output.textContent = "";
+        btn.textContent = "Restart Game!";
+        main.appendChild(btn);
+
+        btn.addEventListener("click", (e) => {
+            yourScore = 0;
+            computerScore = 0;
+            userChoice.textContent = "";
+            botChoice.textContent = "";
+            finalResult.textContent = "";
+            
+            humanScore.textContent = "";
+            botScore.textContent = "";
+        });
+    }
+
+    
     
 
 });
@@ -82,3 +106,17 @@ function playRound(humanChoice, computerChoice){
     }
 }
 
+function isWinner(){
+    
+    if(yourScore > computerScore){
+        finalResult.textContent = `You won the game by final score: You: ${yourScore} to Bot: ${computerScore}`;
+        finalResult.setAttribute("style", "color: green; text-transform: uppercase;");
+
+    }else if (yourScore < computerScore){
+        finalResult.textContent = `You lost the game by score: You: ${yourScore} to Bot: ${computerScore}`;
+        finalResult.setAttribute("style", "color: red; text-transform: uppercase;");
+    }else {
+        finalResult.textContent = `The game got tied! final score: You: ${yourScore} to Bot: ${computerScore}`;
+        finalResult.setAttribute("style", "color: red; text-transform: uppercase;");
+    }
+}
